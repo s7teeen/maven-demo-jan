e0c74aeb06b2834a4c018d4e802a30489fd9c02c
s7teeen <s7teeen@gmail.com>
2026-09-23 11:50:52 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static String getMessage() {
+        return "Hello from Maven + Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
