de8461dd9954ca796b41b4c3e2ae583e0e17bd53
s7teeen <s7teeen@gmail.com>
2026-09-23 11:53:02 +0530

    Create AppTest.java

---
+++ src/java/com/example/AppTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testGetMessage() {
+        assertEquals(
+            "Hello from Maven + Jenkins!",
+            App.getMessage()
+        );
+    }
+}
